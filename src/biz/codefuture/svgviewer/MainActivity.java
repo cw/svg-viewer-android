d36aa006b8ab3b0dd583a7d2dd119a69f753ac6b
Christian Wilcox <cw@christianwilcox.com>
2013-01-20 13:41:08 -0600

    basic funtioning svg viewer works

--- src/biz/codefuture/svgviewer/MainActivity.java
+++ src/biz/codefuture/svgviewer/MainActivity.java
@@ -1,15 +1,30 @@
 package biz.codefuture.svgviewer;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.webkit.WebView;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+	
+	private static final String TAG = "SVGViewerActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Intent intent = getIntent();
+	    Uri data = intent.getData();
+	    Log.v(TAG, "data=" + data);
+
+	    if (data != null && data.toString().indexOf("file") > -1) {	
+	    	WebView webview = (WebView) findViewById(R.id.webView1);
+	    	webview.loadUrl(data.toString());
+	    }
 	}
 
 	@Override
@@ -18,5 +33,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-
 }
